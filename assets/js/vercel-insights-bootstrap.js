44efc71028f3ffb2762345ac4841582f7ccb5ca8
sparshverma <sparshv48@gmail.com>
2026-04-05 19:40:32 +0530

    optimization

---
+++ assets/js/vercel-insights-bootstrap.js
@@ -0,0 +1,7 @@
+window.va = window.va || function () {
+  (window.vaq = window.vaq || []).push(arguments);
+};
+
+window.si = window.si || function () {
+  (window.siq = window.siq || []).push(arguments);
+};
